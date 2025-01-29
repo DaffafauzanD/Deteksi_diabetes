d6e3e5fbb9196b42c5d2c600896f9757037e45d1
DaffafauzanD <daffafauzand24@gmail.com>
2025-01-29 21:56:08 +0700

    update app.py to set dynamic port for deployment

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ def index():
         return render_template('hasil.html', finalData=isDiabetes)
     else:
         return render_template('index.html')
-
+        
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=True, host='0.0.0.0', port=port)
