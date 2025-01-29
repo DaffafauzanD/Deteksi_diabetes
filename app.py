2965bc7096906b4cbe46c4c186b9dfe12b337bda
DaffafauzanD <daffafauzand24@gmail.com>
2025-01-29 22:16:02 +0700

    simplify app.py by removing dynamic port configuration

--- app.py
+++ app.py
@@ -28,8 +28,6 @@ def index():
         return render_template('hasil.html', finalData=isDiabetes)
     else:
         return render_template('index.html')
-        
+
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get('PORT', 8080))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
